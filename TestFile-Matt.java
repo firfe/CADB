f6da6536793c8945755fa12970b0cc667c964e97
MatthewErickson <ericks51@students.wwu.edu>
2016-01-19 14:20:40 -0800

    Matt: Added test file

---
+++ TestFile-Matt.java
@@ -0,0 +1,9 @@
+/* Matthew Erickson
+ * January 19, 2016
+ * TestFile-Matt.java
+ * Use: Ensuring I can successfully add files
+ */
+
+public static void main(String[] args){
+    System.out.println("Hello World");
+}
